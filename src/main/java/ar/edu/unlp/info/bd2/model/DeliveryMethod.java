a9202285a0152f86236a573cbe52d70e7907a31e
Juan Cruz <juancrujca@gmail.com>
2021-04-26 17:41:29 -0300

    +DeliveryMethod class

--- src/main/java/ar/edu/unlp/info/bd2/model/DeliveryMethod.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/DeliveryMethod.java
@@ -1,5 +1,53 @@
 package ar.edu.unlp.info.bd2.model;
 
 public class DeliveryMethod {
+	private Integer id;
+	private String name;
+	private Integer cost;
+	private float startWeight;
+	private float endWeight;
+	
+	public DeliveryMethod(String name, Integer cost, float startWeight, float endWeight) {
+		this.name = name;
+		this.cost = cost;
+		this.startWeight = startWeight;
+		this.endWeight = endWeight;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+
+	public float getStartWeight() {
+		return startWeight;
+	}
+
+	public void setStartWeight(float startWeight) {
+		this.startWeight = startWeight;
+	}
+
+	public float getEndWeight() {
+		return endWeight;
+	}
+
+	public void setEndWeight(float endWeight) {
+		this.endWeight = endWeight;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
 }
