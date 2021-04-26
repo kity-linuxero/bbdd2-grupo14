12cac448dfdf64cef57f561ed0b92c9058b9c3c2
Juan Cruz <juancrujca@gmail.com>
2021-04-26 17:21:08 -0300

    +Provider class

--- src/main/java/ar/edu/unlp/info/bd2/model/Provider.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Provider.java
@@ -1,5 +1,39 @@
 package ar.edu.unlp.info.bd2.model;
 
 public class Provider {
+	
+	private Integer id;
+	private String name;
+	private String cuit;
+	
+	public Provider(Integer id, String name, String cuit) {
+		this.id = id;
+		this.name = name;
+		this.cuit = cuit;
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCuit() {
+		return cuit;
+	}
+
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+	
 }
