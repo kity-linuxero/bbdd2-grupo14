374907fe301bcb95199121b498a326d84c134b98
Juan Cruz <juancrujca@gmail.com>
2021-04-26 18:20:10 -0300

    +OnDeliveryPayment class

---
+++ src/main/java/ar/edu/unlp/info/bd2/model/OnDeliveryPayment.java
@@ -0,0 +1,36 @@
+package ar.edu.unlp.info.bd2.model;
+
+public class OnDeliveryPayment extends Payment {
+	
+	private Integer id;
+	private String name;
+	private float promisedAmount;
+	
+	public OnDeliveryPayment(String name, float promisedAmount) {
+		super();
+		this.name = name;
+		this.promisedAmount = promisedAmount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPromisedAmount() {
+		return promisedAmount;
+	}
+
+	public void setPromisedAmount(float promisedAmount) {
+		this.promisedAmount = promisedAmount;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	
+}
