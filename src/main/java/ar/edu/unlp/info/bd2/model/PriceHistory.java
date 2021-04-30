450d669a1f5896629a7fd77fa1bf1dd0b2120a5d
Bases de datos 2 <--global>
2021-04-30 15:52:59 -0300

    Sumamos clase PriceHistory

---
+++ src/main/java/ar/edu/unlp/info/bd2/model/PriceHistory.java
@@ -0,0 +1,48 @@
+package ar.edu.unlp.info.bd2.model;
+
+import java.util.Date;
+
+public class PriceHistory {
+	
+	private Long id;
+	private Date dateFrom;
+	private Date dateTo;
+	private Provider provider;
+	private Product product;
+	
+	public PriceHistory(Date dateFrom, Date dateTo, Provider provider, Product product) {
+		this.dateFrom = dateFrom;
+		this.dateTo = dateTo;
+		this.provider = provider;
+		this.product = product;
+	}
+	
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+	public Date getDateTo() {
+		return dateTo;
+	}
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+	public Provider getProvider() {
+		return provider;
+	}
+	public void setProvider(Provider provider) {
+		this.provider = provider;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+}
