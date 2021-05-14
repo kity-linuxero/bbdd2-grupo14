1d16404f61464753751c5500e94540ebe0320d77
kity-linuxero <cgiambruni@gmail.com>
2021-05-14 20:05:50 -0300

    Updates scripts

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 mysql -u root -p -e "drop database if exists grupo14;
                   create database grupo14;
+                  CREATE USER IF NOT EXISTS 'grupo'@'localhost' IDENTIFIED BY 'pa$$word';
+                  GRANT ALL PRIVILEGES ON grupo14. * TO 'grupo14'@'localhost';
                   FLUSH PRIVILEGES;
                   "
                   
